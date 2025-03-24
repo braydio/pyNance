458f259438c5ee285a8b2f3954127d703a2275db
braydio <chaffee.brayden@gmail.com>
2025-03-24 02:56:11 -0400

    Localhost binding 00000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0", // bind on all network interfaces
+    port: 3353, 
     proxy: {
-        "/api": {
+      "/api": {
             target: "http://127.0.0.1:5000", // Flask backend
             changeOrigin: true,
             rewrite: (path) => path.replace(/^\/api/, "/api"),
