1786237f19895bc8d7381f21c4d0c252192bef23
braydio <chaffee.brayden@gmail.com>
2025-04-19 09:19:42 +0000

    2025-04-19 - __init__.py - v1.2.0

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -1,11 +1,20 @@
-
 # backend/app/config/__init__.py
 
 from .log_setup import setup_logger
 from .plaid_config import plaid_client
 from .environment import (
-    PLAID_CLIENT_NAME, PLAID_CLIENT_ID, PLAID_SECRET, PLAID_ENV, PLAID_BASE_URL,
-    TELLER_API_BASE_URL, TELLER_APP_ID, VARIABLE_ENV_TOKEN, VARIABLE_ENV_ID, PRODUCTS
+    PLAID_CLIENT_NAME,
+    PLAID_CLIENT_ID,
+    PLAID_SECRET,
+    PLAID_ENV,
+    PLAID_BASE_URL,
+    TELLER_API_BASE_URL,
+    TELLER_APP_ID,
+    TELLER_CERTIFICATE,
+    TELLER_PRIVATE_KEY,
+    VARIABLE_ENV_TOKEN,
+    VARIABLE_ENV_ID,
+    PRODUCTS,
 )
 from .constants import FILES, TELEMETRY, DIRECTORIES, SQLALCHEMY_DATABASE_URI
 from .paths import BASE_DIR
@@ -15,4 +24,4 @@ env_check = PLAID_ENV.upper()
 logger = setup_logger()
 logger.debug(f"Loaded config from {__name__}")
 logger.debug(f"Starting dashboard in Plaid {PLAID_ENV} Environment.")
-logger.debug("======")
+
