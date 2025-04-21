5c3831bd4f38f5160e42a7d7efc0cdc579515b4b
braydio <chaffee.brayden@gmail.com>
2025-04-20 20:37:50 -0400

    2025-04-20 - __init__.py - v1.6.0

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,27 +1,30 @@
 # File: app/__init__.py
 
-from app.config import logger, plaid_client
-from app.extensions import db
 from flask import Flask
 from flask_cors import CORS
-from flask_migrate import Migrate  # New import
+from flask_migrate import Migrate
+
+from app.config import logger, plaid_client
+from app.extensions import db
+from app.config.environment import TELLER_WEBHOOK_SECRET
 
 
 def create_app():
     app = Flask(__name__)
 
+    # Core setup
     CORS(app)
     app.config.from_object("app.config")
     db.init_app(app)
-    Migrate(app, db)  # Initialize Flask-Migrate
+    Migrate(app, db)
 
+    # Import & execute setup tasks
     with app.app_context():
         from app.routes.categories import refresh_plaid_categories
 
         refresh_plaid_categories()
-        # Ensure models are imported so that tables are registered
-        # db.create_all()
-        # Import and register blueprints
+
+        # Import blueprints
         from app.routes.transactions import transactions
         from app.routes.accounts import accounts
         from app.routes.recurring import recurring
@@ -33,7 +36,9 @@ def create_app():
         from app.routes.plaid_investments import plaid_investments
         from app.routes.plaid_transactions import plaid_transactions
         from app.routes.teller_transactions import teller_transactions
+        from app.routes.teller_webhook import webhooks, disabled_webhooks
 
+        # Register blueprints with prefixes
         app.register_blueprint(export, url_prefix="/api/export")
         app.register_blueprint(categories_bp, url_prefix="/api/categories")
         app.register_blueprint(transactions, url_prefix="/api/transactions")
@@ -42,10 +47,16 @@ def create_app():
         app.register_blueprint(charts, url_prefix="/api/charts")
         app.register_blueprint(recurring, url_prefix="/api/recurring")
         app.register_blueprint(plaid_bp, url_prefix="/api/plaid")
+        app.register_blueprint(plaid_transactions, url_prefix="/api/plaid/transactions")
+        app.register_blueprint(plaid_investments, url_prefix="/api/plaid/investments")
         app.register_blueprint(
             teller_transactions, url_prefix="/api/teller/transactions"
         )
-        app.register_blueprint(plaid_transactions, url_prefix="/api/plaid/transactions")
-        app.register_blueprint(plaid_investments, url_prefix="/api/plaid/investments")
+
+        # Conditionally enable Teller webhook
+        if TELLER_WEBHOOK_SECRET:
+            app.register_blueprint(webhooks, url_prefix="/api/webhooks")
+        else:
+            app.register_blueprint(disabled_webhooks, url_prefix="/api/webhooks")
 
     return app
