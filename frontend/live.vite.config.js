045b1859d0a7b285fb7238bfbc7fc0addd7a3585
braydio <chaffee.brayden@gmail.com>
2025-04-24 04:28:58 -0400

    2025-04-24 - live.vite.config.js - v1.0.0

--- frontend/live.vite.config.js
+++ frontend/live.vite.config.js
@@ -8,15 +8,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   server: {
     host: "0.0.0.0", // bind on all network interfaces
-    port: 3353, 
+    port: 3353,
     proxy: {
       "/api": {
-            target: "http://127.0.0.1:5000", // Flask backend
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, "/api"),
-        },
+        target: "http://127.0.0.1:5000", // Flask backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
     },
-},
+  },
   plugins: [
     vue(),
     vueDevTools(),
