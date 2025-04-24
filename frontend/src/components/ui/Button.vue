d0328f11fcf2ee566350b09b56eb3f91273e7029
braydio <chaffee.brayden@gmail.com>
2025-04-24 08:27:31 -0400

    2025-04-24 - Button.vue - v1.0.0

---
+++ frontend/src/components/ui/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :class="computedClass" :disabled="disabled" @click="$emit('click', $event)">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  variant: {
+    type: String,
+    default: '', // 'outline', 'success', 'alert'
+  },
+  pill: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const computedClass = computed(() => {
+  return [
+    'btn',
+    props.variant && `btn-${props.variant}`,
+    props.pill && 'btn-pill',
+  ]
+    .filter(Boolean)
+    .join(' ');
+});
+</script>
