9d1cc74871564a62f5c18e91ee4d97e3624bfbed
braydio <chaffee.brayden@gmail.com>
2025-05-02 01:38:25 -0400

    2025-05-02 - Forecast.vue - v1.0.0

--- frontend/src/views/Forecast.vue
+++ frontend/src/views/Forecast.vue
@@ -3,10 +3,10 @@
     <ForecastLayout />
   </div>
 </template>
-	
+
 <script setup>
 // Top-level Forecast page view, pulls together full mock forecast framework
-import ForecastLayout from '@components/forecast/ForecastLayout.vue'
+import ForecastLayout from '@/components/forecast/ForecastLayout.vue'
 </script>
 <style scoped>
 /* Layout polish for better spacing */
