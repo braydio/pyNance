c6d8fe608ba329c63ce0d22a07cf311ea7839466
braydio <chaffee.brayden@gmail.com>
2025-05-10 06:33:36 -0400

    2025-05-10 - charts.js - v1.0.0

---
+++ frontend/src/api/charts.js
@@ -0,0 +1,15 @@
+// src/api/transactions.js
+import axios from 'axios'
+
+export const fetchTransactions = async (page = 1, pageSize = 15) => {
+  const response = await axios.get('/api/transactions/get_transactions', {
+    params: { page, page_size: pageSize }
+  })
+  return response.data
+}
+
+export const updateTransaction = async (transactionData) => {
+  const response = await axios.put('/api/transactions/update', transactionData)
+  return response.data
+}
+
