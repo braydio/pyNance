104aca1045b99cd560537158164f3924285b01d5
braydio <chaffee.brayden@gmail.com>
2025-06-04 05:42:27 -0400

    Format helpers with black

--- backend/app/helpers/plaid_exchange_helpers.py
+++ backend/app/helpers/plaid_exchange_helpers.py
@@ -7,15 +7,13 @@ def exchange_public_token_for_product(public_token, product):
     from plaid import Client
     from backend.api.keys import plaid_client_keys
 
-    client = Client(client_id=plaid_client_keys.get("user_id"),
-                secret=plaid_client_keys.get("client_secret"),
-                env=plaid_client_keys.get("env"))
+    client = Client(
+        client_id=plaid_client_keys.get("user_id"),
+        secret=plaid_client_keys.get("client_secret"),
+        env=plaid_client_keys.get("env"),
+    )
 
     resp = client.Items.item_public_token_exchange(public_token)
     access_token = resp.access_token
     item_id = resp.item_id
-    return {
-      "access_token": access_token,
-      "item_id": item_id,
-      "product": product
-    }
+    return {"access_token": access_token, "item_id": item_id, "product": product}
