7a15153601991141eb1ea3c16c2997cc59a14eb2
braydio <chaffee.brayden@gmail.com>
2025-06-09 02:50:21 -0400

    2025-06-09 - __init__.py - v1.0.0

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -1,11 +1,5 @@
 # backend/app/config/__init__.py
 
-from .log_setup import setup_logger
-from .plaid_config import PLAID_BASE_URL, plaid_client
-from .constants import (
-    DATABASE_NAME,
-    SQLALCHEMY_DATABASE_URI,
-)
 from .environment import (
     CLIENT_NAME,
     FLASK_ENV,
