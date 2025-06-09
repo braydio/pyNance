9d789b8587513e713f2b0fbec66f845a56b1882c
braydio <chaffee.brayden@gmail.com>
2025-06-09 14:51:48 -0400

    2025-06-09 - __init__.py - v1.0.0

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -26,6 +26,8 @@ from .constants import (
     SQLALCHEMY_DATABASE_URI,
 )
 from .paths import BASE_DIR
+from .log_setup import setup_logger
+from .plaid_config import PLAID_BASE_URL, plaid_client
 
 env_check = PLAID_ENV.upper()
 
