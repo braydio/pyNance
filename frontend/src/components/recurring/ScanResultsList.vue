2cff1617a7e96a208d105109c492e3188f515523
braydio <93299297+braydio@users.noreply.github.com>
2025-06-09 02:58:06 -0400

    Add frontend components for recurring scan demo (#87)
    * Add frontend components for recurring scan demo
    
    * 2025-06-05 - pre-push - v1.0.0
    
    * 2025-06-05 - test_syntax_check.py - v1.0.0
    
    * 2025-06-05 - charts.py - v1.0.0
    
    * 2025-06-06 - lint_routes.py - v1.1.0
    
    * 2025-06-06 - setup.sh - v1.0.0
    
    * Config fixing
    
    * 2025-06-08 - components.d.ts - v1.0.0
    
    * Config fixing
    
    * 2025-06-08 - test_no_query_scope.py - v1.2.0
    
    * 2025-06-09 - __init__.py - v1.0.0
    
    * 2025-06-09 - charts.py - v1.0.0
    
    * 2025-06-09 - charts.py - v1.1.0

---
+++ frontend/src/components/recurring/ScanResultsList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="list-disc list-inside space-y-1">
+    <li v-for="(action, idx) in results" :key="idx">
+      <span class="font-semibold">ID:</span> {{ action.recurring_id }} -
+      <span class="italic">{{ action.status }}</span>
+    </li>
+    <li v-if="!results.length" class="text-gray-500">No actions detected.</li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  results: { type: Array, default: () => [] }
+});
+</script>
