37ec94084ac51216e99d5cb93ddfd6905e738527
braydio <chaffee.brayden@gmail.com>
2025-08-03 15:04:20 -0400

    M

--- backend/app/routes/transactions.py
+++ backend/app/routes/transactions.py
@@ -143,7 +143,6 @@ def get_transactions_paginated():
         start_date = (
             datetime.strptime(start_date_str, "%Y-%m-%d") if start_date_str else None
         )
-        end_date = datetime.strptime(end_date_str, "%Y-%m-%d") if end_date_str else None
 
         transactions, total = account_logic.get_paginated_transactions(
             page,
