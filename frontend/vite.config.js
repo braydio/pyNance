6d5360df45a3821dac61dfbda2a1c934df1ff421
braydio <chaffee.brayden@gmail.com>
2025-09-19 03:18:35 -0400

    cloudflare allow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -76,6 +76,7 @@ export default defineConfig(async () => {
     server: {
       allowedHosts: [
         '.ngrok-free.app',
+        '.trycloudflare.com',
       ],
       host: '0.0.0.0',
       port: 3353,
