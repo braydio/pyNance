a643f455ff545f85d9c2fed88f94a07d00b8a9e7
braydio <93299297+braydio@users.noreply.github.com>
2025-11-18 02:41:56 -0500

    test(transactions): wait for end-date request

--- frontend/cypress/e2e/transactions.cy.js
+++ frontend/cypress/e2e/transactions.cy.js
@@ -36,6 +36,10 @@ describe('Transactions View', () => {
 
     // Date range filter
     cy.get('input[type="date"]').first().type('2024-01-01')
+    cy.wait('@getTx').its('request.query').should('include', {
+      start_date: '2024-01-01',
+    })
+
     cy.get('input[type="date"]').eq(1).type('2024-01-31')
     cy.wait('@getTx').its('request.query').should('include', {
       start_date: '2024-01-01',
